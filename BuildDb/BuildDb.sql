1a155065819f1d2351c6d2f064b82fb37097ed99
Mayank Shekhar <mshekhar@vmware.com>
2018-11-27 00:09:58 +0530

    Added script to build db

---
+++ BuildDb/BuildDb.sql
@@ -0,0 +1,73 @@
+-- Create database named Matdaan and then create the following tables.
+    CREATE TABLE VOTING(
+        VOTER_ID NVARCHAR (20) NOT NULL,
+        NAME NVARCHAR (50) NOT NULL,
+        FATHER_NAME NVARCHAR (50) NOT NULL,
+        DOB NVARCHAR(10) NOT NULL,
+        GENDER NVARCHAR(10) NOT NULL,
+        LINKED_TO_AADHAR BIT DEFAULT(0),
+        VOTED_FOR NVARCHAR(30),
+        CONSTRAINT [PK_VOTERID] PRIMARY KEY CLUSTERED (VOTER_ID ASC),
+    );
+
+    CREATE TABLE AADHAR(
+        AADHAR_NO NVARCHAR(12) NOT NULL,
+        NAME NVARCHAR(50) NOT NULL,
+        FATHER_NAME NVARCHAR(50) NOT NULL,
+        DOB NVARCHAR(50) NOT NULL,
+        GENDER NVARCHAR(10) NOT NULL,
+        CONTACT_NO NVARCHAR(10),
+        EMAIL_ID NVARCHAR(50),
+        OTP INT,
+        OTP_VERIFIED BIT DEFAULT(0),
+        VOTER_ID NVARCHAR(20),
+        CONSTRAINT [PK_AADHAR_NO] PRIMARY KEY CLUSTERED (AADHAR_NO ASC),
+    );
+
+/* Insert dummy data
+USE [Matdaan]
+GO
+
+INSERT INTO [dbo].[VOTING]
+           ([VOTER_ID]
+           ,[NAME]
+           ,[FATHER_NAME]
+           ,[DOB]
+           ,[GENDER]
+           ,[LINKED_TO_AADHAR]
+           ,[VOTED_FOR])
+     VALUES
+           ('000123456789'
+           ,'Mayank Shekhar'
+           ,'Devendra Kumar Sinha'
+           ,'01-12-1991'
+           ,'Male'
+           ,0
+           ,NULL)
+GO
+
+INSERT INTO [dbo].[AADHAR]
+           ([AADHAR_NO]
+           ,[NAME]
+           ,[FATHER_NAME]
+           ,[DOB]
+           ,[GENDER]
+           ,[CONTACT_NO]
+           ,[EMAIL_ID]
+           ,[OTP]
+           ,[OTP_VERIFIED]
+           ,[VOTER_ID])
+     VALUES
+           ('123456789000'
+           ,'Mayank Shekhar'
+           ,'Devendra Kumar Sinha'
+           ,'01-12-1991'
+           ,'Male'
+           ,'8951471517'
+           ,'zmsrms@gmail.com'
+           ,NULL
+           ,0
+           ,NULL)
+GO
+
+*/
